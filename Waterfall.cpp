5a73496866579a114e8157c445c90246a7893a68
Timothy Krause <tmkrs4482@gmail.com>
2025-05-05 11:49:16 -0600

    Removed a texture glitch.

--- Waterfall.cpp
+++ Waterfall.cpp
@@ -151,8 +151,8 @@ void Waterfall::InitQuads(void)
         glBindTexture(GL_TEXTURE_2D, textures[t]);
         glTexStorage2D(GL_TEXTURE_2D, 1,
                        GL_RG8, Npoints, Nlines);
-        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
-        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+        glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
         glTexParameteri( GL_TEXTURE_2D, GL_AUTO_GENERATE_MIPMAP, GL_FALSE );
